c96ec98849f2c13ea6ed7aab87629aeab274e15f
Nina <nina05022002@mail.ru>
2021-01-10 03:57:40 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/* alert('Hello'); */
+
+/* const result = confirm("Are you here?");
+console.log(result); */
+
+/* const answer = prompt("Вам есть 18?", "18");
+console.log(typeof(answer)); */
+
+const answers = [];
+
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Сколько вам лет?', '');
+answers[2] = prompt('В каком городе вы проживаете?', '');
+
+console.log(typeof(answers));
+console.log(typeof(null));
